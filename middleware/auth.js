7b4b1054a0222c2c7d394566b4d2bfd98cca43f6
Maruthi K J <123438736+Maruthi2896@users.noreply.github.com>
2023-09-04 06:58:31 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,8 @@
-
+export const auth = async (token) => {
+  try {
+    const id = await jwt.verify(token, process.env.SECRETE_KEY);
+    return id;
+  } catch (err) {
+    res.send({ error: err.message });
+  }
+};
